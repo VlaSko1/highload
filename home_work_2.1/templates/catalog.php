4c418af3aa6cb46396aaecab260edf860970f9ed
Vladimir <Vizion84@yandex.ru>
2021-02-14 13:24:57 +0300

    Solving homework for the fifth lesson.

--- home_work_2.1/templates/catalog.php
+++
@@ -1,8 +0,0 @@
-<h2>Каталог товаров:</h2>
-<?php foreach ($catalog as $value):?>
-<div>
-    <h2><?=$value['name']?></h2>
-    Стоимость:  <?=$value['price']?><br>
-    <button>Купить</button> <hr>
-</div>
-<?php endforeach;?>
--- home_work_2.1/templates/menu.php
+++
@@ -1,5 +0,0 @@
-<a href="/">Главная</a>
-<a href="/?page=catalog">Каталог</a>
-<a href="/?page=about">О нас</a>
-<a href="/?page=gallery">Галлерея</a>
-<a href="/?page=apicatalog">api Test</a><br>
